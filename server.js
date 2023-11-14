c02df3f4005f584f199a832cebf95374a09b764e
maor-israel <77739201+maor-israel@users.noreply.github.com>
2023-11-14 09:25:52 +0200

    wertg

--- server.js
+++ server.js
@@ -1,18 +1,39 @@
-const express = require("express");
+const express = require('express');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
 const app = express();
+const port = process.env.PORT || 3000;
 
+// Middleware to parse the incoming requests with JSON payloads
+app.use(express.json());
 
-app.post("/", (req, res) => {
+// Endpoint for the webhook
+app.get('/', (req, res) => {
+    // Your verify token. Should be a secret kept in the environment variables
+    const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
-    const body = req.body;
-    console.log(body)
-    res.send(
-        "banana"
-    )
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+
+    // Checks if a token and mode are in the query string of the request
+    if (mode && token) {
+        // Checks the mode and token sent are correct
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+            // Responds with the challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+        } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);      
+        }
+    }
 });
 
-
-
-
-
-app.listen(1000, () => console.log("started"))
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});

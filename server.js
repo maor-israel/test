02e22e24357c6b4520440e86695d979bf52b4f86
maor-israel <77739201+maor-israel@users.noreply.github.com>
2023-11-14 09:17:55 +0200

    df

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ const express = require("express");
 const app = express();
 
 
-app.post("/", (req,res) => {
+app.post("/", (req, res) => {
 
-    const body = JSON.parse(req.body);
+    const body = req.body;
     console.log(body)
-    res.send(200)
+    res.send(
+        "banana"
+    )
 });
 
 
 
 
 
-app.listen(1000,() => console.log("started"))
+app.listen(1000, () => console.log("started"))

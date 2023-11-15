71192931d3b245f3f082cc6f48972353bd1401a7
maor-israel <77739201+maor-israel@users.noreply.github.com>
2023-11-15 09:23:40 +0200

    erg

--- server.js
+++ server.js
@@ -12,6 +12,31 @@ app.use(express.json());
 
 // Endpoint for the webhook
 app.get('/', (req, res) => {
+    console.log("req",req);
+    // Your verify token. Should be a secret kept in the environment variables
+    const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+
+    // Checks if a token and mode are in the query string of the request
+    if (mode && token) {
+        // Checks the mode and token sent are correct
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+            // Responds with the challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+        } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);      
+        }
+    }
+});
+
+app.post('/', (req, res) => {
+    console.log("req",req);
     // Your verify token. Should be a secret kept in the environment variables
     const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 

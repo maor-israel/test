feee60feec36a59bbe39212b755e28238395bc1b
maor-israel <77739201+maor-israel@users.noreply.github.com>
2023-11-16 01:22:47 +0200

    k

--- server.js
+++ server.js
@@ -4,16 +4,13 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 1000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.get('/get', (req, res) => {
     const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
-    console.log("msg", req.body.entry[0]);
-
-    // var textMessage = req.body.entry[0].changes[0].value.messages[0];
-    // console.log("msg", JSON.stringify(textMessage));
+    console.log("msg", req.body);
 
     let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
@@ -29,5 +26,24 @@ app.get('/get', (req, res) => {
     }
 });
 
+app.post('/get', (req, res) => {
+    const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+    console.log("msg", req.body.entry[0].changes[0].value.messages[0].text.body);
 
-app.listen(port, () => console.log("started"))
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+
+    if (mode && token) {
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+        } else {
+            res.sendStatus(403);
+        }
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});

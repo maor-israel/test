381edc148e9027040c2c96ac108f69bf56c92e39
maor-israel <77739201+maor-israel@users.noreply.github.com>
2023-11-15 11:40:38 +0200

    erth

--- server.js
+++ server.js
@@ -8,8 +8,25 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// Endpoint for the webhook
 app.get('/get', (req, res) => {
+    const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+    console.log("msg", req.body);
+
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+
+    if (mode && token) {
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+        } else {
+            res.sendStatus(403);
+        }
+    }
+});
+
+app.post('/get', (req, res) => {
     const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
     console.log("msg", req.body);
 

f818c0f2a436793131f5f9cac73054a2390c274a
maor-israel <77739201+maor-israel@users.noreply.github.com>
2023-11-16 00:43:43 +0200

    d

--- server.js
+++ server.js
@@ -25,27 +25,3 @@ app.get('/get', (req, res) => {
         }
     }
 });
-
-app.post('/get', (req, res) => {
-    const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
-    console.log("msg - ", JSON.stringify(req.body));
-
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
-
-
-
-    if (mode && token) {
-        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-            console.log('WEBHOOK_VERIFIED');
-            res.status(200).send(challenge);
-        } else {
-            res.sendStatus(403);
-        }
-    }
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});

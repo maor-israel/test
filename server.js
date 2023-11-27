ade8d64e56c9e96a419818fbb9ab1ba1565a1f5d
maor-israel <77739201+maor-israel@users.noreply.github.com>
2023-11-27 03:58:44 +0200

    l

--- server.js
+++ server.js
@@ -20,47 +20,19 @@ app.get("/webhook", async (req, res) => {
 });
 
 app.post("/webhook", async (req, res) => {
-    try {
-      const body = req.body;
-      const from = body.entry[0].changes[0].value.messages[0].from;
-      const text = body.entry[0].changes[0].value.messages[0].text.body;
-      const type = body.entry[0].changes[0].value.messages[0].type;
+  try {
+    const body = req.body;
+    const from = body.entry[0].changes[0].value.messages[0].from;
+    const text = body.entry[0].changes[0].value.messages[0].text.body;
+    const type = body.entry[0].changes[0].value.messages[0].type;
 
-      console.log({ from, text, type });
+    console.log({ from, text, type });
 
-      res.status(200).send("Message processed");
-    } catch (error) {
-      console.error("Error processing message:", error);
-      res.status(500).send("Internal Server Error");
-    }
-});
-
-app.get("/send", async (req, res) => {
-  const main = async () => {
-    const res = await fetch(
-      "https://graph.facebook.com/v17.0/107553652069379/messages",
-      {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
-          "Content-Type": " application/json",
-        },
-        body: JSON.stringify({
-          messaging_product: "whatsapp",
-          to: "972505551142",
-          type: "template",
-          template: { name: "hello_world", language: { code: "en_US" } },
-        }),
-      }
-    );
-
-    const json = await res.json();
-    return json;
-  };
-
-  const result = await main();
-
-  res.send(result);
+    res.status(200).send("Message processed");
+  } catch (error) {
+    console.error("Error processing message:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 app.listen(port, () => {
